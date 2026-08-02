88ddb289a7c6e92bc60e42fac34789a706d5c187
Adam <adamidrissi177@gmail.com>
2026-08-02 11:54:15 +0100

    Added description

--- Monte_Carlo.h
+++ Monte_Carlo.h
@@ -26,3 +26,46 @@ public:
 };
 
 #endif
+
+/*
+The base equation is e^-rT * right side.
+
+Right side depends on if it is a call or put
+
+Call right side = 1/m (M top, i=1 bottom) sum_of(S(i)t - k, 0)
+
+Put right side = 1/m (M top, i=1 bottom) sum_of(k - S(i)t, 0)
+
+S(i)t is a function which takes i representing what number simulation it is.
+
+S(i)t = S0 * e^(r - 1/2 O^2)*T + O*sqrt(T)*Z
+
+Z represents a random number from 0 - 1
+
+S0 basically just represents current price
+
+We have two parts left and right.
+
+Left: the adjusted annual growth.
+
+(r - 1/2 O^2)*T we have two parts here the r - risk free rate and 1/2 O^2 meaning volatility correction
+
+So when you minus both you get adjusted growth then times it by Time getting annual growth.
+
+Right: This gives Volatility * sqrt(time) * Random number.
+
+O * sqrt(T) = O^2 * T = Variance * T = Spread of numbers from mean * T
+
+O * sqrt(T) so that is where we get sqrt(t) we are using this form to get the spread of possible returns
+which is standard deviation.
+
+Z the random number changes the direction and strength when we times that to O we are going to change
+the standard deviation getting a different stock price.
+
+The output gives a log return and that e converts it to a normal multiplyer.
+
+Then we times that to the stock getting random numbers.
+
+After we minus that new price to k or reverse depending on call or put
+
+*/
